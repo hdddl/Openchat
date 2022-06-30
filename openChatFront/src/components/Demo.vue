cce40447d4758a241878d64f46a8285b0cdef1cc
huangdongliu <dongliu@dongliu.site>
2022-06-30 17:24:51 +0800

    完成前端显示

--- openChatFront/src/components/Demo.vue
+++ openChatFront/src/components/Demo.vue
@@ -1,93 +1,74 @@
+<template>
+  <div class="flex justify-center">
+    <div id="log" class="flex-col flex-wrap w-1/2">
+
+    </div>
+  </div>
+  <div class="flex justify-center py-5 mt-10">
+    <div class="fixed bottom-5">
+      <input autofocus type="text" id="msg" size="64" class="border-solid rounded-lg shadow-xl py-2 text-center bg-slate-100" @keypress="entryPress"/>
+      <button @click="sendMessage" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded mx-4">Send</button>
+    </div>
+  </div>
+</template>
+
+
 <script>
-var msg
-var log
-var conn
+let msg;
+let log;
+let conn;
 
-function init(){
-    msg = document.getElementById("msg");
-    log = document.getElementById("log");
-    conn = new WebSocket("ws://localhost:8080/ws");
-}
-
-// 发送socket消息
-function sendMessage(){
-    if(!conn){
-        appendlog("<b>Connection closed.<b>")
-        return false;
-    }
-    appendlog('<p style="color:blue;text-align: right"> ' + msg.value +' <p>');
-    conn.send(msg.value);
-    msg.value = "";
-    return false;
-}
-
-function entryPress(event){
-    if(event.key == "Enter"){
-        event.preventDefault();
-        sendMessage();
-    }
-}
-
-function appendlog(message){
-    let item = document.createElement("div");
-    item.innerHTML =  message;
-    log.appendChild(item);
-}
-
-function connect(){
-    conn.onclose = function(evt){
-        appendlog("<b>Connection closed.</b>");
-    };
-    conn.onmessage = function(evt){
-        var recvMessage = evt.data.split('\n')
-        for(var i = 0; i < recvMessage.length; i++){
-            appendlog(recvMessage[i]);
-        }
-    }
+function appendLog(message){
+  let item = document.createElement("div");
+  let breakLine = `<div class="py-4">
+        <div class="w-full border-t border-gray-300"></div>
+      </div>`
+  item.innerHTML =  message + breakLine;
+  log.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 export default{
     methods:{
-        appendlog,
-        sendMessage,
-        entryPress,
+      // 发送socket消息
+      sendMessage(){
+          if(conn.readyState === 3 || conn.readyState === 2){    // 当websocket处于关闭状态
+            appendLog("<b>Connection closed.<b>")
+            console.log("Connection closed")
+            return false;
+          }
+          if(msg.value === ""){     // 不发送空字符串
+            return false;
+          }
+          appendLog('<div class="text-right mt-5"> ' + msg.value + '</div>');   // 自己发送的消息
+          conn.send(msg.value);     // 将消息发送的服务器
+          msg.value = "";     // 清空聊天框
+          return false;
+        },
+        entryPress(event){
+          if(event.key === "Enter"){
+            event.preventDefault();
+            this.sendMessage();
+          }
+        },
     },
-    mounted(){
-        init();     // 变量初始化
-        connect();      // 连接websockets
+    mounted(){    // 变量初始化并设置回调函数
+        msg = document.getElementById("msg");
+        log = document.getElementById("log");
+        conn = new WebSocket("ws://localhost:8080/ws");
+        conn.onclose = function(evt){
+          appendLog("<b>Connection closed.</b>");
+        };
+        conn.onmessage = function(evt){
+            let receiveMessage = evt.data.split('\n')
+            for(let i = 0; i < receiveMessage.length; i++) {
+              let messageHTML = '<p class="justify-center mt-5"> ' + receiveMessage[i] +' <p>'   // 别入发送过来的消息
+              appendLog(messageHTML);
+            }
+        }
     }
 }
 </script>
 
-<template>
-    <div id="log">
-    </div>
-    <div id="form">
-        <input type="text" id="msg" size="64" @keypress="entryPress"/>
-        <input type="submit" @click="sendMessage" value="Sent" />
-    </div>
-</template>
 
-<style>
-#log {
-    background: white;
-    margin: 0;
-    padding: 0.5em 0.5em 0.5em 0.5em;
-    position: absolute;
-    top: 0.5em;
-    left: 0.5em;
-    right: 0.5em;
-    bottom: 3em;
-    overflow: auto;
-}
-#form {
-    padding: 0 0.5em 0 0.5em;
-    margin: 0;
-    position: absolute;
-    bottom: 1em;
-    left: 0px;
-    width: 100%;
-    overflow: hidden;
-}
-</style>
 
